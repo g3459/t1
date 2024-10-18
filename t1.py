9968f6b7db8f40d1d07ac98bdd325551fb0ab595
roberto <r.aracil@nayarsystems.com>
2024-10-18 13:16:18 +0200

    asdasd

--- t1.py
+++ t1.py
@@ -1,30 +1,17 @@
 import hashlib
-import string
-
-base=62
-nstr=string.ascii_lowercase+string.ascii_uppercase+'0123456789'
 
 source=str(input('source: '))
 sourcehash=hashlib.sha256(source.encode('utf-8')).hexdigest()
 sourceint=int(sourcehash,16)
 encoding=str(input('encoding: '))
-estr=''
-ap=0
-for e in encoding.split():
-    for ch in nstr:
-        if ch==e[0]:
-            ap=1
-        if ap==1:
-            estr+=ch
-        if ch==e[1]:
-            ap=0
-fstr=''
+base=len(encoding)
+
 while sourceint>=base:
     rest=sourceint % base
-    fstr=estr[rest]+fstr
+    fstr=encoding[rest]+fstr
     sourceint=sourceint//base
 
-fstr=estr[sourceint]+fstr
-fstr256hash=hashlib.sha256(fstr.encode('utf-8')).hexdigest()
+fstr=encoding[sourceint]+fstr
 print('fstr: '+fstr)
+fstr256hash=hashlib.sha256(fstr.encode('utf-8')).hexdigest()
 print('sha256hash: '+fstr256hash)
